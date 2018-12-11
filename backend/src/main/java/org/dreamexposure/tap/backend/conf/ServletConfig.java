4f33d5b63dfbb8f04b806f668108f00cfb7d1aac
NovaFox161 <novamaday@gmail.com>
2018-12-11 17:18:49 -0600

    Get CORS to play nice.

--- backend/src/main/java/org/dreamexposure/tap/backend/conf/ServletConfig.java
+++ backend/src/main/java/org/dreamexposure/tap/backend/conf/ServletConfig.java
@@ -6,6 +6,10 @@ import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author NovaFox161
@@ -18,11 +22,23 @@ import org.springframework.http.HttpStatus;
 @Configuration
 @EnableAutoConfiguration
 public class ServletConfig {
+    
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
         return (container -> {
             container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
             container.setPort(Integer.valueOf(SiteSettings.PORT.get()));
         });
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                CorsRegistration reg = registry.addMapping("/v1/**");
+                reg.allowedOrigins("https://www.startapped.com");
+            }
+        };
     }
 }
