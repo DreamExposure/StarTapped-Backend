ac20ada81b4a785f740dda718e127198311befab
NovaFox161 <novamaday@gmail.com>
2019-02-28 12:52:27 -0600

    Blog creation on the backend with file copying is now implemented.

--- backend/src/main/java/org/dreamexposure/tap/backend/utils/FileHandler.java
+++ backend/src/main/java/org/dreamexposure/tap/backend/utils/FileHandler.java
@@ -1,7 +1,9 @@
 package org.dreamexposure.tap.backend.utils;
 
+import org.apache.commons.io.FileUtils;
 import org.dreamexposure.tap.backend.conf.SiteSettings;
 import org.dreamexposure.tap.core.objects.blog.IBlog;
+import org.dreamexposure.tap.core.utils.Logger;
 
 import java.io.File;
 
@@ -13,14 +15,28 @@ import java.io.File;
  * Company Website: https://www.dreamexposure.org
  * Contact: nova@dreamexposure.org
  */
-@SuppressWarnings("UnnecessaryLocalVariable")
 public class FileHandler {
     public static boolean createDefaultBlogFolders(IBlog blog) {
-        
-        boolean success = (new File(SiteSettings.BLOG_FOLDER.get() + "/" + blog.getBaseUrl() + "/public")).mkdirs();
-        
-        //TODO: Create default .html file and everything else that is stupid important!!!!!!!!
-        
+
+        boolean success = false;
+
+        try {
+            boolean s = (new File(SiteSettings.BLOG_FOLDER.get() + "/" + blog.getBaseUrl() + "/public")).mkdirs();
+
+            if (s) {
+                File source = new File(SiteSettings.TEMPLATE_FOLDER.get() + "/blog/index.html");
+                File dest = new File(SiteSettings.BLOG_FOLDER.get() + "/" + blog.getBaseUrl() + "/public/index.html");
+
+                FileUtils.copyFile(source, dest);
+
+                success = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.getLogger().exception("Failed to create blog directory and file", e, FileHandler.class);
+            success = false;
+        }
+
         return success;
     }
 }
