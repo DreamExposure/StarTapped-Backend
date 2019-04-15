c1826698f92b09e55dd41b7f84bc516e3cc6ca6b
NovaFox161 <novamaday@gmail.com>
2019-04-15 15:07:54 -0500

    Better handling of email templates.

--- backend/src/main/java/org/dreamexposure/tap/backend/network/email/EmailHandler.java
+++ backend/src/main/java/org/dreamexposure/tap/backend/network/email/EmailHandler.java
@@ -2,8 +2,17 @@ package org.dreamexposure.tap.backend.network.email;
 
 import net.sargue.mailgun.Configuration;
 import net.sargue.mailgun.Mail;
+import org.apache.commons.io.IOUtils;
 import org.dreamexposure.tap.core.conf.GlobalVars;
 import org.dreamexposure.tap.core.conf.SiteSettings;
+import org.dreamexposure.tap.core.utils.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
 
 /**
  * @author NovaFox161
@@ -13,8 +22,13 @@ import org.dreamexposure.tap.core.conf.SiteSettings;
  * Company Website: https://www.dreamexposure.org
  * Contact: nova@dreamexposure.org
  */
+@Component
 public class EmailHandler {
     private static EmailHandler instance;
+
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     private Configuration mailgunConfig;
     
@@ -37,25 +51,30 @@ public class EmailHandler {
     }
     
     public void sendEmailConfirm(String emailTo, String confirmationLink) {
-        Mail.using(mailgunConfig)
-                .to(emailTo)
-                .subject("Confirm Your Email")
-                .html(getConfirmEmail(emailTo, confirmationLink))
-                .build()
-                .sendAsync();
+        try {
+            String emailHtml = getConfirmEmail(emailTo, confirmationLink);
+            Mail.using(mailgunConfig)
+                    .to(emailTo)
+                    .subject("Confirm Your Email")
+                    .html(emailHtml)
+                    .build()
+                    .sendAsync();
+        } catch (Exception e) {
+            Logger.getLogger().exception("Failed to load and send confirmation email", e, true, this.getClass());
+        }
     }
-    
-    private String getConfirmEmail(String emailTo, String confirmationLink) {
-        //TODO: FIX STYLING TO MATCH SITE!!!!!!!!!!!!!!!!!!
-        //TODO: Make this actually better and like readable and shit!!!!
-        return "<h1 style=\"text-align: center; color: #de1a1a;\">Confirm Your Email</h1>\n" +
-                "<p style=\"text-align: center;\">Thank you for signing up for an account at <a title=\"" + GlobalVars.name + "\" href=\"" + GlobalVars.siteUrl + "\" target=\"_blank\">" + GlobalVars.siteUrl + "</a></p>\n" +
-                "<p style=\"text-align: center;\">Please click the button below to confirm your email.</p>\n" +
-                "<p style=\"text-align: center;\">Didn't sign up? Just ignore this message.</p>\n" +
-                "<p style=\"text-align: center;\"><a href=\"" + confirmationLink + "\"><button style=\"font-size: 18px; background-color: #de1a1a; color: white; padding: 10px; border: 2px black; margin: 10px; width: auto; height: auto;\">Confirm Email</button></a></p>\n" +
-                "<p style=\"text-align: center;\">&nbsp;Button not working? Click the link below:</p>\n" +
-                "<p style=\"text-align: center;\"><a title=\"Confirm Email\" href=\"" + confirmationLink + "\" target=\"_blank\">" + confirmationLink + "</a></p>\n" +
-                "<p style=\"text-align: center;\">Email sent to " + emailTo + " in response to account creation</p>\n" +
-                "<p style=\"text-align: center;\">&copy; 2018 <a title=\"DreamExposure\" href=\"https://www.dreamexposure.org\" target=\"_blank\">DreamExposure</a> | <a title=\"Privacy Policy\" href=\"https://dreamexposure.org/policy/privacy\" target=\"_blank\">Privacy Policy</a> | <a title=\"Contact\" href=\"https://www.dreamexposure.org/contact\" target=\"_blank\">Contact</a></p>";
+
+    private String getConfirmEmail(String emailTo, String confirmationLink) throws IOException {
+        return loadEmailResource("account-confirmation.html")
+                .replaceAll("%SITE_URL", GlobalVars.siteUrl)
+                .replaceAll("%CONFIRMATION_LINK", confirmationLink)
+                .replaceAll("%EMAIL_TO", emailTo)
+                .replaceAll("%YEAR", Calendar.getInstance().get(Calendar.YEAR) + "");
+    }
+
+
+    private String loadEmailResource(String fileName) throws IOException {
+        InputStream stream = resourceLoader.getResource("classpath:email/" + fileName).getInputStream();
+        return IOUtils.toString(stream, "UTF-8");
     }
 }
